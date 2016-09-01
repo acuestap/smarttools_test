695104eeb5517eda3d578deafe56e90df1611a9d
Abimelec Cuesta <acuestap@gmail.com>
2016-09-01 04:43:45 -0500

    Merge branch 'master' into develop-a
    # Conflicts:
    #	competitions/__pycache__/business_logic.cpython-35.pyc
    #	competitions/__pycache__/urls.cpython-35.pyc
    #	competitions/__pycache__/views.cpython-35.pyc
    #	web/__pycache__/models.cpython-35.pyc

--- web/models.py
+++ web/models.py
@@ -1,31 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.forms import ModelForm, ImageField
+from rest_framework import serializers
 
 
 class Competition(models.Model):
     name = models.CharField(max_length=200, null=False)
-    image = models.CharField(max_length=200, null=False)
+    image = models.ImageField(upload_to='upload_files/competitions/images', null=False)
     url = models.CharField(max_length=200, null=False)
     startingDate = models.DateTimeField(null=False)
     deadline = models.DateTimeField(null=False)
     description = models.CharField(max_length=200, null=False)
     user = models.ForeignKey(User, null=False)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class CompetitionForm(serializers.ModelSerializer):
+
+    class Meta:
+        model = Competition
+        read_only_fields = ('name', 'url', 'description', 'startingDate', 'deadline')
 
 
 class Video(models.Model):
     name = models.CharField(max_length=200, null=False)
     state = models.CharField(max_length=30, null=False)
     user_email = models.CharField(max_length=30, null=False)
-    uploadDate = models.DateTimeField(null=False)
+    uploadDate = models.DateTimeField(null=True)
     message = models.CharField(max_length=200, null=False)
     competition = models.ForeignKey(Competition, null=False)
-    original_video = models.FileField(upload_to='video', null=True)
-    convertido = models.FileField(upload_to='video', blank=True, null=True)
-    converted = models.BooleanField(default=False)
+    original_video = models.FileField(upload_to='videos', null=True)
+    converted_video = models.FileField(upload_to='videos', blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+
+class VideoForm(ModelForm):
+    class Meta:
+        model = Video
+        fields = ['name', 'user_email', 'message','original_video']
--- web/static/src/modules/competitions/competition.svc.js
+++ web/static/src/modules/competitions/competition.svc.js
@@ -3,11 +3,18 @@
 
     mod.service('competitionService', ['$http', 'competitionContext', function ($http, context) {
 
-
         this.getCompetitions = function () {
             return $http({
                 method: 'GET',
                 url: '/competitions'
+            });
+        };
+
+        this.registerCompetition = function (competition) {
+            return $http({
+                method: 'POST',
+                url: 'competition/create/',
+                data: competition
             });
         };
 
