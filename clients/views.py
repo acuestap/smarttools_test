03a7a902464f07b1171ed85cfbac61deb577f75a
Abimelec Cuesta <acuestap@gmail.com>
2016-09-02 17:34:22 -0500

    CU Presentación Concursos
    Ajuste para que tome de manera dinámica el nombre del cliente y liste
    solo los concursos que tiene asociados.

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,9 @@
+import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from clients.business_logic import register_client_in_model
+from web.models import User
 
 @csrf_exempt
 def register_client(request):
@@ -10,3 +12,33 @@ def register_client(request):
 
         return JsonResponse(response)
 
+
+@csrf_exempt
+def get_info_client(request):
+    if request.user.is_authenticated():
+        id_user = request.user.id
+        user = User.objects.get(id=id_user)
+        first_name = user.first_name
+        last_name = user.last_name
+        email = user.email
+
+        jsonData = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email
+        }
+
+        return JsonResponse(jsonData)
+'''
+@csrf_exempt
+def manage_clients(request):
+    if request.method == 'GET':
+        jsonData = json.loads(request.body.decode('utf-8'))
+        client = User.objects.get(id=jsonData['pk'])
+        client.first_name = jsonData.get('first_name')
+        client.last_name = jsonData.get('last_name')
+        client.username = jsonData.getname('')
+        client.email = jsonData.get('email')
+
+        return JsonResponse({"data": client})
+'''
